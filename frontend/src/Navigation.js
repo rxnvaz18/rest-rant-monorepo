af9412dbde2ec3e414e37316f3a6f8547625ba77
unknown <roxanvaz31@gmail.com>
2024-03-11 01:04:40 -0700

    now only edit and delete buttons show on place details page if the user is an admin

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -30,7 +30,18 @@ function Navigation() {
             </li>
         )
     }
+    let addPlaceButton = null
 
+    if (currentUser?.role === 'admin') {
+        addPlaceButton = (
+            <li>
+                <a href="#" onClick={() => history.push("/places/new")}>
+                    Add Place
+                </a>
+            </li>
+        )
+    }
+    
     return (
         <nav>
             <ul>
@@ -49,7 +60,9 @@ function Navigation() {
                         Add Place
                     </a>
                 </li>
+                {addPlaceButton}
                 {loginActions}
+                
             </ul>
         </nav>
     )
--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -100,8 +100,21 @@ function PlaceDetails() {
 			)
 		})
 	}
+	let placeActions = null
 
-
+	if (currentUser?.role === 'admin') {
+		placeActions = (
+			<>
+				<a className="btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>
+				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
+	
 	return (
 		<main>
 			<div className="row">
@@ -128,6 +141,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
+					{placeActions}
 					<a className="btn btn-warning" onClick={editPlace}>
 						Edit
 					</a>{` `}
