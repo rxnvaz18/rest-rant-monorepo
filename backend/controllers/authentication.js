1720687e856db3727a7efd8fea61cdc331d259f4
unknown <roxanvaz31@gmail.com>
2024-03-10 23:59:21 -0700

    added jwt package and updated authentication file in controllers to add jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,8 +1,23 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: user
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 
 router.post('/', async (req, res) => {
     
@@ -15,8 +30,9 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
-    }
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })           
+        res.json({ user: user, token: result.value })           
+       }
 })
 
 
@@ -24,4 +40,5 @@ module.exports = router
 
   
 
-  
+
+    
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,18 +1,19 @@
 import { createContext, useState } from "react";
 
 
-export const CurrentUser = createContext()
+export const CurrentUser = createContext() 
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
-    window.setCurrentUser = setCurrentUser
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+    useEffect(() => {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
 }
-
 export default CurrentUserProvider
-
