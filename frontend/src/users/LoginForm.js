fe28cb251d50bdfbab29cfa5c533d7b4513762d5
unknown <roxanvaz31@gmail.com>
2024-03-11 00:07:04 -0700

    updated handle submit code to add jwt to local storage

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,21 +17,30 @@ function LoginForm() {
   
   
     async function handleSubmit(e) {
-        const response = await fetch(`http://localhost:5000/authentication/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
+        e.preventDefault();
     
-        const data = await response.json()
+        try {
+            const response = await fetch(`http://localhost:5000/authentication/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(credentials)
+            });
     
-        if (response.status === 200) {
-            setCurrentUser(data.user)
-            history.push(`/`)
-        } else {
-            setErrorMessage(data.message)
+            const data = await response.json();
+    
+        
+            if (response.ok) {  
+                setCurrentUser(data.user);
+                localStorage.setItem('token', data.token);
+                history.push(`/`);
+            } else {
+                setErrorMessage(data.message);
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+            setErrorMessage('An error occurred, please try again');
         }
     }
 
@@ -80,3 +89,5 @@ function LoginForm() {
 }
 
 export default LoginForm
+
+ 
