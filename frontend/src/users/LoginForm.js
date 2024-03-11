d2e5ce7a97b088916bf75b43f8b89203d1cbf9ae
unknown <roxanvaz31@gmail.com>
2024-03-10 23:30:34 -0700

    updated authentication file to see the associated user record logged in back-end terminal

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -14,12 +14,23 @@ function LoginForm() {
     })
 
     const [errorMessage, setErrorMessage] = useState(null)
+  
+async function handleSubmit(e) {
+    e.preventDefault()
+    const response = await fetch(`http://localhost:5000/authentication/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-       
+    const data = await response.json()
 
-    }
+    console.log(data)
+}
+  
+
 
     return (
         <main>
