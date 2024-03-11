039a1401eeff14339493798373de6642ff3eaae3
unknown <roxanvaz31@gmail.com>
2024-03-11 00:11:34 -0700

    added user object on the contents of jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -7,12 +7,27 @@ const { User } = db
 
 router.get('/profile', async (req, res) => {
     try {
-        let user = await User.findOne({
-            where: {
-                userId: user
-            }
-        })
-        res.json(user)
+        // Split the authorization header into [ "Bearer", "TOKEN" ]:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // Only handle "Bearer" authorization for now 
+        //  (we could add other authorization strategies later):
+        if (authenticationMethod == 'Bearer') {
+
+            // Decode the JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Get the logged in user's id from the payload
+            const { id } = result.value
+
+            // Find the user object using their id:
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
     } catch {
         res.json(null)
     }
@@ -38,7 +53,7 @@ router.post('/', async (req, res) => {
 
 module.exports = router
 
-  
+
 
 
     
